ce7c32111e8b25dbad2f0643ba8b3060ca6064f6
Thomas Mauran <thomasmauran@yahoo.com>
2026-01-29 13:39:06 +0100

    chore: bump chess-tui 2.4.0
    Signed-off-by: Thomas Mauran <thomasmauran@yahoo.com>

--- Formula/chess-tui.rb
+++ Formula/chess-tui.rb
@@ -1,27 +1,27 @@
 class ChessTui < Formula
   desc "Play chess from your terminal 🦀"
   homepage "https://github.com/thomas-mauran/chess-tui"
-  version "2.3.0"
+  version "2.4.0"
   license "MIT"
   head "https://github.com/thomas-mauran/chess-tui.git", branch: "main"
   
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/thomas-mauran/chess-tui/releases/download/2.3.0/chess-tui-2.3.0-aarch64-apple-darwin.tar.gz"
-      sha256 "ea30131651863d90e994d6a8f9fcb1dff04d21284331e50ee41d1052a0464e75"
+      url "https://github.com/thomas-mauran/chess-tui/releases/download/2.4.0/chess-tui-2.4.0-aarch64-apple-darwin.tar.gz"
+      sha256 "6272975ec82a4dc107940328e002fdc62a9f2b0ef907670ad5f51387c7f021c3"
     else
-      url "https://github.com/thomas-mauran/chess-tui/releases/download/2.3.0/chess-tui-2.3.0-x86_64-apple-darwin.tar.gz"
-      sha256 "336da70a72543882afe872a9ec5898446ab70138b806fd377243e1a6c56249ba"
+      url "https://github.com/thomas-mauran/chess-tui/releases/download/2.4.0/chess-tui-2.4.0-x86_64-apple-darwin.tar.gz"
+      sha256 "65bf66dec20a5a621d6ff9e1f085606a4868ae1d6a7dbf68a8d261e45f8fb22d"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/thomas-mauran/chess-tui/releases/download/2.3.0/chess-tui-2.3.0-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "4a40b46cf0a09c8c5b71b541b421dc3ced656b58e186e6849da480db09fc33ea"
+      url "https://github.com/thomas-mauran/chess-tui/releases/download/2.4.0/chess-tui-2.4.0-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "95140471f334bf63e5b0d467f4f3d79ae685b11a9f8f03cfabff3eb63ba93009"
     else
-      url "https://github.com/thomas-mauran/chess-tui/releases/download/2.3.0/chess-tui-2.3.0-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "c94c4b972b6a833144af6f33276c93ff4b5ebbfd47eac76d57b887a4c120f312"
+      url "https://github.com/thomas-mauran/chess-tui/releases/download/2.4.0/chess-tui-2.4.0-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "5e54243ee239fec4bc6f70d3671faac2991ca2546197ec0c1c2811be3dc313a6"
     end
   end
 
